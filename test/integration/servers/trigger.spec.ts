6229585256bf82c26ce18116562c8e4265737e23
Juan P Lopez <dolcalmi@gmail.com>
2021-11-02 11:13:50 -0500

    Test sub block height validation

--- test/integration/servers/trigger.spec.ts
+++ test/integration/servers/trigger.spec.ts
@@ -84,8 +84,10 @@ describe("onchainBlockEventhandler", () => {
     const initialBlock = await bitcoindClient.getBlockCount()
     const subBlocks = subscribeToBlocks({ lnd: lnd1 })
     subBlocks.on("block", async ({ height }) => {
-      await onchainBlockEventhandler({ height })
-      lastHeight = height > lastHeight ? height : lastHeight
+      if (height > lastHeight) {
+        lastHeight = height > lastHeight ? height : lastHeight
+        await onchainBlockEventhandler({ height })
+      }
       isFinalBlock = lastHeight >= initialBlock + blocksToMine
     })
 
